176e935b47cddfdfb0b884376742d720890f9e43
Sarah Baskin <sarahbaskin@brandeis.edu>
2025-10-01 19:13:17 -0400

    creating gameplay interaction

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Main file that runs gameplay
+'''
+
+
+def setup():
+    pass
+
+def play():
+    pass
+
+def endGame():
+    pass
+
+if __name__ == "__main__":
+    setup()
+    play()
+    endGame()
